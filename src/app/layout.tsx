09c282ba10a342f5a3171c6264ecb3f5d983c7f6
Luna Santos <lunafeyli@gmail.com>
2023-09-16 15:13:27 -0300

    :chart_with_upwards_trend: Add google analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,20 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
+import ReactGA from "react-ga4";
 
 export const metadata: Metadata = {
 	title: "MultiQSMP",
 };
+
+ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID!);
+
+ReactGA.send({
+	hitType: "pageview",
+	page: "/pt",
+	title: "Acesso em Português",
+});
+ReactGA.send({ hitType: "pageview", page: "/en", title: "Acesso em Inglês" });
 
 export default async function RootLayout({
 	children,
