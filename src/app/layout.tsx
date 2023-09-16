355c782ee509442ad3bbef66d437f19268d12be6
Luna Santos <lunafeyli@gmail.com>
2023-09-16 15:27:51 -0300

    :chart_with_upwards_trend: Fix GA (AGAIN)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,11 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
-import ReactGA from "react-ga4";
+import Script from "next/script";
 
 export const metadata: Metadata = {
 	title: "MultiQSMP",
 };
-
-ReactGA.initialize("G-P0V7XD4TFG");
-
-// ReactGA.send({
-// 	hitType: "pageview",
-// 	page: "/pt",
-// 	title: "Acesso em Português",
-// });
-// ReactGA.send({ hitType: "pageview", page: "/en", title: "Acesso em Inglês" });
-ReactGA.send({ hitType: "pageview", page: "/pt" });
 
 export default async function RootLayout({
 	children,
@@ -24,6 +14,15 @@ export default async function RootLayout({
 }) {
 	return (
 		<html>
+			<Script id="google-tag-manager" strategy="afterInteractive">
+				{`
+					(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+					})(window,document,'script','dataLayer','G-P0V7XD4TFG');
+					`}
+			</Script>
 			<body className={"min-h-screen h-full overflow-x-hidden"}>
 				{children}
 				<Analytics />
